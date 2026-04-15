3471afa63722b68b51b61b54da1c58686a1118ef
Abraham Hernandez <cahernandez.abraham@gmail.com>
2026-04-15 11:43:47 -0700

    Added tests and fixed bugs in Password class

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -16,7 +16,56 @@ class PasswordTest : public ::testing::Test
 
 TEST(PasswordTest, single_letter_password)
 {
-	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+    Password p;
+    ASSERT_EQ(1, p.count_leading_characters("Z"));
+}
+
+TEST(PasswordTest, empty_string)
+{
+    Password p;
+    ASSERT_EQ(0, p.count_leading_characters(""));
+}
+
+TEST(PasswordTest, all_same_characters)
+{
+    Password p;
+    ASSERT_EQ(4, p.count_leading_characters("aaaa"));
+}
+
+TEST(PasswordTest, stops_on_different_char)
+{
+    Password p;
+    ASSERT_EQ(2, p.count_leading_characters("aabb"));
+}
+
+TEST(PasswordTest, no_repetition)
+{
+    Password p;
+    ASSERT_EQ(1, p.count_leading_characters("abcd"));
+}
+
+/* -------- has_mixed_case tests -------- */
+
+TEST(PasswordTest, mixed_case_true)
+{
+    Password p;
+    ASSERT_TRUE(p.has_mixed_case("Abc"));
+}
+
+TEST(PasswordTest, all_lowercase)
+{
+    Password p;
+    ASSERT_FALSE(p.has_mixed_case("abc"));
+}
+
+TEST(PasswordTest, all_uppercase)
+{
+    Password p;
+    ASSERT_FALSE(p.has_mixed_case("ABC"));
+}
+
+TEST(PasswordTest, empty_string_mixed_case)
+{
+    Password p;
+    ASSERT_FALSE(p.has_mixed_case(""));
 }
